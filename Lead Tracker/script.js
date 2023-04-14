0ee22390db635fe83fb10834082e9e7c85c3d3d1
Solomon Jude <solomonjude38@gmail.com>
2023-04-13 18:51:53 -0700

    Update script.js

--- Lead Tracker/script.js
+++ Lead Tracker/script.js
@@ -13,7 +13,7 @@
  }
 
  tabBtn.addEventListener('click', function(){
-     chrome.tabs.query({active: true, currentwindow: true}, function(tabs){
+     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
          myLeads.push(tabs[0].url)
          localStorage.setItem('myLeads', JSON.stringify(myLeads))
          render(myLeads)
